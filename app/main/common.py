a93b4ed28aad50bf35b9ca7fa127d308f7fff992
ShenBoWen <952468655@qq.com>
2020-04-07 17:12:03 +0800

    修改获取log日期的正则

--- app/main/common.py
+++ app/main/common.py
@@ -134,7 +134,7 @@ def is_yesday_log(log_name):
     '''判断Log日期是否为昨天
     将当前日期加八个小时'''
 
-    p_log_datetime = r'(\d{4})(\d{2})(\d{2})-(\d{2})(\d{2})(\d{2})-UTC'
+    p_log_datetime = r'(\d{4})(\d{2})(\d{2})-(\d{2})(\d{2})(\d{2})'
     res = re.search(p_log_datetime, log_name)
     real_date_time = datetime.datetime(int(res.group(1)), int(res.group(2)), int(res.group(3)), \
         int(res.group(4)), int(res.group(5)), int(res.group(6))) + datetime.timedelta(hours=8) + datetime.timedelta(days=2)
